546ae0597cee89860e296b0dbce8110d8e10fafa
David Goodine <dmgoodine@gmail.com>
2015-10-20 09:11:56 -0400

    Added more files.

---
+++ Slogger/Slogger/Slogger.h
@@ -0,0 +1,19 @@
+//
+//  Slogger.h
+//  Slogger
+//
+//  Created by David Goodine on 10/20/15.
+//  Copyright © 2015 David Goodine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Slogger.
+FOUNDATION_EXPORT double SloggerVersionNumber;
+
+//! Project version string for Slogger.
+FOUNDATION_EXPORT const unsigned char SloggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Slogger/PublicHeader.h>
+
+
