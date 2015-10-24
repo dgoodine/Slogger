ab96de7a5f78f2bf47a23593775016cb58a73b01
David Goodine <dmgoodine@gmail.com>
2015-10-24 12:32:34 -0400

    Work on testing.

--- Slogger/Slogger/Slogger.swift
+++ Slogger/Slogger/Slogger.swift
@@ -13,7 +13,12 @@ public typealias LogClosure = () -> String
 public protocol SloggerCategory : Hashable {}
 
 public enum Level : Int, Comparable {
-  case None, Severe, Error, Warning, Info, Debug, Verbose
+  case None, Severe, Error, Warning, Info, Debug, Verbose, Trace
+
+  // Wow.  This is pretty dumb.
+  static func allValues () -> [Level] {
+    return [None, Severe, Error, Warning, Info, Debug, Verbose, Trace]
+  }
 }
 
 // Marker protocol for client-provided logging category enums
@@ -23,7 +28,7 @@ public func <<T: RawRepresentable where T.RawValue: Comparable>(a: T, b: T) -> B
 }
 
 public enum Detail : Int {
-  case Time = 0, File, Function, Level, Category
+  case Date, File, Function, Level, Category
 }
 
 public class Slogger <T: SloggerCategory> : NSObject {
@@ -47,37 +52,7 @@ public class Slogger <T: SloggerCategory> : NSObject {
 
     self.currentLevel = defaultLevel
     self.dateFormatter = df!
-    self.details = (details != nil) ? details! : [.Time, .File, .Function, .Level, .Category]
-  }
-
-  // MARK: Public
-  public func error (function: String = __FUNCTION__, _ file: String = __FILE__, _ line: Int = __LINE__, @autoclosure closure: LogClosure) {
-      logInternal(closure, category: nil, level: .Error, function: function, file: file, line: line)
-  }
-
-  public func error (category: T? = nil, @autoclosure _ closure: LogClosure,
-     _ function: String = __FUNCTION__, _ file: String = __FILE__, _ line: Int = __LINE__) {
-    logInternal(closure, category: category, level: .Error, function: function, file: file, line: line)
-  }
-
-  public func warning (@autoclosure closure: LogClosure,
-    category: T? = nil, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__)  {
-    logInternal(closure, category: category, level: .Warning, function: function, file: file, line: line)
-  }
-
-  public func info (@autoclosure closure: LogClosure,
-    category: T? = nil, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__)  {
-    logInternal(closure, category: category, level: .Info, function: function, file: file, line: line)
-  }
-
-  public func debug (@autoclosure closure: LogClosure,
-    category: T? = nil, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__)  {
-    logInternal(closure, category: category, level: .Debug, function: function, file: file, line: line)
-  }
-
-  public func verbose (@autoclosure closure: LogClosure,
-    category: T? = nil, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__)  {
-    logInternal(closure, category: category, level: .Verbose, function: function, file: file, line: line)
+    self.details = (details != nil) ? details! : [.Date, .File, .Function, .Category, .Level]
   }
 
   // MARK: Private
@@ -90,42 +65,181 @@ public class Slogger <T: SloggerCategory> : NSObject {
     return level <= operatingLevel
   }
 
-  private func logInternal (@autoclosure closure: LogClosure, category: T?, level: Level, function: String, file: String, line: Int) {
+  func logInternal (condition: Bool, @noescape _ closure: LogClosure, category: T?, level: Level, function: String, file: String, line: Int) {
 
-      guard canLog(category, level: level) else {
-        return;
+    guard canLog(category, level: level) else {
+      return;
+    }
+
+    let string = closure()
+    let str : NSMutableString = NSMutableString()
+    str.appendString("-")
+
+    func maybeSpace () {
+      if str.length > 0 {
+        str.appendString(" ")
       }
+    }
 
-      let string = closure()
-      let str : NSMutableString = NSMutableString()
-      for detail in details {
-        switch detail {
-        case .Category:
-          if category != nil {
-            str.appendString("\(category!)")
-          }
+    for detail in details {
+      maybeSpace()
 
-        case .File:
-          let f = __FILE__
-          str.appendString(f)
-          break
+      switch detail {
 
-        case .Function:
-          str.appendString(function)
-          break
-
-        case .Level:
-          break
-
-        case .Time:
-          break
+      case .Category:
+        if category != nil {
+          str.appendString("[\(category!)]")
+        } else {
+          str.appendString("[]")
         }
+
+      case .File:
+        var f = file as NSString
+        f = f.lastPathComponent
+        str.appendString(f as String)
+        str.appendString(":")
+        str.appendString("\(line)")
+
+      case .Function:
+        str.appendString(function)
+
+      case .Level:
+        str.appendString("\(level)")
+
+      case .Date:
+        let dateString = dateFormatter.stringFromDate(NSDate())
+        str.appendString("[\(dateString)]")
       }
-      
-      str.appendString(": ")
-      str.appendString(string)
-      print(str)
+    }
+
+    str.appendString(": ")
+    str.appendString(string)
+    print(str)
   }
+}
+
+// MARK: - Log site functions
+extension Slogger {
+  // MARK: Severe
+  public func severe (@autoclosure  closure: LogClosure, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__) {
+    logInternal(true, closure, category: nil, level: .Severe, function: function, file: file, line: line)
+  }
+
+  public func severe (category: T?, @autoclosure _ closure: LogClosure, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__) {
+    logInternal(true, closure, category: category, level: .Severe, function: function, file: file, line: line)
+  }
+
+  public func severe (function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__, @noescape closure: LogClosure) {
+    logInternal(true, closure, category: nil, level: .Severe, function: function, file: file, line: line)
+  }
+
+  public func severe (category: T?, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__, @noescape closure: LogClosure) {
+    logInternal(true, closure, category: category, level: .Severe, function: function, file: file, line: line)
+  }
+
+  // MARK: Error
+  public func error (@autoclosure  closure: LogClosure, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__) {
+    logInternal(true, closure, category: nil, level: .Error, function: function, file: file, line: line)
+  }
+
+  public func error (category: T?, @autoclosure _ closure: LogClosure, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__) {
+    logInternal(true, closure, category: category, level: .Error, function: function, file: file, line: line)
+  }
+
+  public func error (function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__, @noescape closure: LogClosure) {
+    logInternal(true, closure, category: nil, level: .Error, function: function, file: file, line: line)
+  }
+
+  public func error (category: T?, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__, @noescape closure: LogClosure) {
+    logInternal(true, closure, category: category, level: .Error, function: function, file: file, line: line)
+  }
+
+  // MARK: Warning
+  public func warning (@autoclosure  closure: LogClosure, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__) {
+    logInternal(true, closure, category: nil, level: .Warning, function: function, file: file, line: line)
+  }
+
+  public func warning (category: T?, @autoclosure _ closure: LogClosure, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__) {
+    logInternal(true, closure, category: category, level: .Warning, function: function, file: file, line: line)
+  }
+
+  public func warning (function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__, @noescape closure: LogClosure) {
+    logInternal(true, closure, category: nil, level: .Warning, function: function, file: file, line: line)
+  }
+
+  public func warning (category: T?, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__, @noescape closure: LogClosure) {
+    logInternal(true, closure, category: category, level: .Warning, function: function, file: file, line: line)
+  }
+
+  // MARK: Info
+  public func info (@autoclosure  closure: LogClosure, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__) {
+    logInternal(true, closure, category: nil, level: .Info, function: function, file: file, line: line)
+  }
+
+  public func info (category: T?, @autoclosure _ closure: LogClosure, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__) {
+    logInternal(true, closure, category: category, level: .Info, function: function, file: file, line: line)
+  }
+
+  public func info (function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__, @noescape closure: LogClosure) {
+    logInternal(true, closure, category: nil, level: .Info, function: function, file: file, line: line)
+  }
+
+  public func info (category: T?, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__, @noescape closure: LogClosure) {
+    logInternal(true, closure, category: category, level: .Info, function: function, file: file, line: line)
+  }
+
+  // MARK: Debug
+  public func debug (@autoclosure  closure: LogClosure, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__) {
+    logInternal(true, closure, category: nil, level: .Debug, function: function, file: file, line: line)
+  }
+
+  public func debug (category: T?, @autoclosure _ closure: LogClosure, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__) {
+    logInternal(true, closure, category: category, level: .Debug, function: function, file: file, line: line)
+  }
+
+  public func debug (function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__, @noescape closure: LogClosure) {
+    logInternal(true, closure, category: nil, level: .Debug, function: function, file: file, line: line)
+  }
+
+  public func debug (category: T?, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__, @noescape closure: LogClosure) {
+    logInternal(true, closure, category: category, level: .Debug, function: function, file: file, line: line)
+  }
+
+  // MARK: Verbose
+  public func verbose (@autoclosure  closure: LogClosure, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__) {
+    logInternal(true, closure, category: nil, level: .Verbose, function: function, file: file, line: line)
+  }
+
+  public func verbose (category: T?, @autoclosure _ closure: LogClosure, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__) {
+    logInternal(true, closure, category: category, level: .Verbose, function: function, file: file, line: line)
+  }
+
+  public func verbose (function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__, @noescape _ closure: LogClosure) {
+    logInternal(true, closure, category: nil, level: .Verbose, function: function, file: file, line: line)
+  }
+
+  public func verbose (category: T?, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__, @noescape _ closure: LogClosure) {
+    logInternal(true, closure, category: category, level: .Verbose, function: function, file: file, line: line)
+  }
+
+  // MARK: Trace
+  public func trace (condition: Bool, @autoclosure  _ closure: LogClosure, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__) {
+    logInternal(condition, closure, category: nil, level: .Trace, function: function, file: file, line: line)
+  }
+
+  public func trace (category: T?, _ condition: Bool, @autoclosure _ closure: LogClosure, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__) {
+    logInternal(condition, closure, category: category, level: .Trace, function: function, file: file, line: line)
+  }
+
+  public func trace (condition: Bool, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__, @noescape closure: LogClosure) {
+    logInternal(condition, closure, category: nil, level: .Trace, function: function, file: file, line: line)
+  }
+
+  public func trace (category: T?, _ condition: Bool, function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__, @noescape closure: LogClosure) {
+    logInternal(condition, closure, category: category, level: .Trace, function: function, file: file, line: line)
+  }
+
+
 }
 
 
--- Slogger/SloggerTests/SloggerTests.swift
+++ Slogger/SloggerTests/SloggerTests.swift
@@ -13,6 +13,10 @@ class SloggerTests: XCTestCase {
 
   enum TestCategory : String, SloggerCategory {
     case First, Second, Third
+
+    static func allValues () -> [TestCategory] {
+      return [First, Second, Third]
+    }
   }
 
   class TestLogger : Slogger<TestCategory> {
@@ -32,19 +36,130 @@ class SloggerTests: XCTestCase {
   }
 
   func testBasic () {
-    log.error() { return "Error" }
-    log.error(.First, "Error")
+    log.error("Message")
+
+    log.error(.First, "Message")
 
     log.error() {
-      return "ClosureError"
+      return "Closure Message"
+    }
+
+    log.error(.First) {
+      return "Closure Message"
+    }
+
+    log.verbose("Message")
+
+    log.verbose(.First, "Message")
+
+    log.verbose() {
+      return "Closure Message"
+    }
+
+    log.verbose(.First) {
+      return "Closure Message"
     }
   }
 
-//  func testPerformanceExample() {
-//    // This is an example of a performance test case.
-//    self.measureBlock {
-//      // Put the code you want to measure the time of here.
-//    }
-//  }
+  func testExhaustive () {
 
+    let levels = Level.allValues()
+    let categories = TestCategory.allValues()
+    var callCount = 0
+
+    func checkString (condition: Bool, _ category: TestCategory?, _ level: Level) {
+      // XCTAssert here
+    }
+
+    func checkClosure (condition: Bool, _ category: TestCategory?, _ level: Level) {
+      // XCTAssert here
+    }
+
+    func callIt (category: TestCategory?, _ level: Level) {
+      switch (level) {
+      case .None:
+        checkString(false, category, level)
+        checkClosure(false, category, level)
+        break
+
+      case .Severe:
+        log.severe(category, "String")
+        checkString(true, category, level)
+        log.severe(category) { return "Closure" }
+        checkClosure(true, category, level)
+
+      case .Error:
+        log.error(category, "String")
+        checkString(true, category, level)
+        log.error(category) { return "Closure" }
+        checkClosure(true, category, level)
+        break
+
+      case .Warning:
+        log.warning(category, "String")
+        checkString(true, category, level)
+        log.warning(category) { return "Closure" }
+        checkClosure(true, category, level)
+        break
+
+      case .Info:
+        log.info(category, "String")
+        checkString(true, category, level)
+        log.info(category) { return "Closure" }
+        checkClosure(true, category, level)
+        break
+
+      case .Debug:
+        log.debug(category, "String")
+        checkString(true, category, level)
+        log.debug(category) { return "Closure" }
+        checkClosure(true, category, level)
+        break
+
+      case .Verbose:
+        log.verbose(category, "String")
+        checkString(true, category, level)
+        log.verbose(category) { return "Closure" }
+        checkClosure(true, category, level)
+        break
+
+      case .Trace:
+        for condition in [true, false] {
+          log.trace(category, condition, "String")
+          checkString(true, category, level)
+          log.trace(category, condition) { return "Closure" }
+          checkClosure(true, category, level)
+        }
+        break
+      }
+
+      callCount++
+    }
+
+    func sloggit (category: TestCategory?) {
+      for level in levels {
+        callIt(category, level)
+      }
+    }
+
+    for setLevel in levels {
+      print("Setting log level: \(setLevel)")
+      log.currentLevel = setLevel
+
+      sloggit(nil)
+      for category in categories {
+        sloggit(category)
+      }
+    }
+
+    print("Call count: \(callCount)")
+  }
+
+  //  func testPerformanceExample() {
+  //    // This is an example of a performance test case.
+  //    self.measureBlock {
+  //      // Put the code you want to measure the time of here.
+  //    }
+  //  }
+  
 }
