bf75fa2d951e26920479732b39d70b9e7d1afb34
David Goodine <dmgoodine@gmail.com>
2015-10-25 13:43:33 -0400

    Add watch target and shared schemes.

---
+++ Slogger/SloggerWatch/SloggerWatch.h
@@ -0,0 +1,19 @@
+//
+//  SloggerWatch.h
+//  SloggerWatch
+//
+//  Created by David Goodine on 10/25/15.
+//  Copyright © 2015 David Goodine. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for SloggerWatch.
+FOUNDATION_EXPORT double SloggerWatchVersionNumber;
+
+//! Project version string for SloggerWatch.
+FOUNDATION_EXPORT const unsigned char SloggerWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SloggerWatch/PublicHeader.h>
+
+
