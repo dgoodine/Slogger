1b98e68532ad46a8b5182cce3114b5ed7e840b74
David Goodine <dmgoodine@gmail.com>
2015-11-10 08:31:23 -0500

    Experimenting with using Slogger from ObjC.

---
+++ Slogger/SloggerTests/SloggerTestsObjC.h
@@ -0,0 +1,13 @@
+//
+//  SloggerTestsObjC.h
+//  Slogger
+//
+//  Created by David Goodine on 11/10/15.
+//  Copyright © 2015 David Goodine. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SloggerTestsObjC : XCTestCase
+
+@end
---
+++ Slogger/SloggerTests/SloggerTestsObjC.m
@@ -0,0 +1,16 @@
+//
+//  SloggerTestsObjC.m
+//  Slogger
+//
+//  Created by David Goodine on 11/10/15.
+//  Copyright © 2015 David Goodine. All rights reserved.
+//
+
+#import "SloggerTestsObjC.h"
+@import Slogger;
+
+//static Slogger *log = Slogger.logObjC;
+
+@implementation SloggerTestsObjC
+
+@end
